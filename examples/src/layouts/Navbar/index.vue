8ed53a169d5e648ba9828d7f84ed8d185632f7b7
Kirk Lin <linkirk@163.com>
2023-08-27 12:16:32 +0800

    feat: add examples

---
+++ examples/src/layouts/Navbar/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineOptions({
+  name: "Navbar",
+});
+</script>
+
+<template>
+  <div
+    class="sticky top-0 z-30 flex h-16 w-full justify-center text-base-content opacity-90 backdrop-blur transition-all duration-100"
+  >
+    HEADER
+  </div>
+</template>
---
+++ examples/src/pages/home/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { presetPalettes } from "@kirklin/palette";
+
+defineOptions({
+  name: "Home",
+});
+console.log(presetPalettes);
+</script>
+
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<style scoped></style>
